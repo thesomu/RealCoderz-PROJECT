91d5f31bcd352cc4923afcf40b31e481312616b7
Jai <jaiarora5080@gmail.com>
2021-05-28 14:37:12 +0530

    secomd commit

--- bill_logs/utils.py
+++ bill_logs/utils.py
@@ -1,11 +1,15 @@
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
+import logging
 
 from xhtml2pdf import pisa
+logger = logging.getLogger('django')
+
 
 #funtion used to render pdf
 def render_to_pdf(template_src, context_dict={}):
+    logger.info("balance sheet pdf generated")
     template = get_template(template_src)
     html = template.render(context_dict)
     result = BytesIO()
